f6c32b5ce8911d1d913b9dd67b8d90115000b8b5
Rasheed M <r.muslimani@gmail.com>
2018-09-20 15:14:49 -0400

    ScrollToBottom feature added

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function ScrollToBottom () {
+	//Selectors
+	var messages = jQuery('#messages');
+	var newMessage = messages.children('li:last-child');
+	//Heights
+	var clientHeight = messages.prop('clientHeight');
+	var scrollTop = messages.prop('scrollTop');
+	var scrollHeight = messages.prop('scrollHeight');
+	var newMessageHeight = newMessage.innerHeight();
+	var lastMessageHeight = newMessage.prev().innerHeight();
+	
+	if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+		messages.scrollTop(scrollHeight);
+	}
+}
+
 socket.on('connect', function () {
 	console.log('Connected to Server');	
 });
@@ -20,6 +36,7 @@ socket.on('newMessage', function(message) {
 	});
 	
 	jQuery('#messages').append(html);
+	ScrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -33,6 +50,7 @@ socket.on('newLocationMessage', function(message) {
 	});
 	
 	jQuery('#messages').append(html);
+	ScrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
